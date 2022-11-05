456508487c99754fd43935771486d25655f891a8
singhbhupinder651 <71844883+singhbhupinder651@users.noreply.github.com>
2022-11-05 19:31:50 +0530

    Create MoodAnalysisException.cs

---
+++ MoodAnalysisException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserExceptionProblem
+{
+    public class MoodAnalysisException : ApplicationException
+    {
+        public enum ExceptionType
+        {
+            NULL_MESSAGE,
+            EMPTY_MESSAGE,
+            NO_SUCH_FIELD,
+            NO_SUCH_METHOD,
+            NO_SUCH_CLASS,
+            OBJECT_CREATION_ISSUE,
+            MESSAGE_NOT_FOUND
+        }
+
+        public readonly ExceptionType type;
+
+        public MoodAnalysisException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
